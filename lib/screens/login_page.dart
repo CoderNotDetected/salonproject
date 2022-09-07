e1f3390f63d606efadca5bbfb3d324a104c8ff10
CoderNotDetected <parashhaobam3@gmail.com>
2022-09-07 15:48:22 +0530

    new

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,33 +3,16 @@ import 'package:barberapp/screens/sign_up.dart';
 import 'package:flutter/material.dart';
  
 
-class LoginPage extends StatelessWidget {
+
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-  static const String _title = 'Hair Salon';
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: _title,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
-      ),
-    );
-  }
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _LoginPageState extends State<LoginPage> {
 
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -37,85 +20,101 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: ListView(
-      children: <Widget>[
-        Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(10), 
-          child: Text(
-            'Hair Salon',
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.w500,
-              fontSize: 30,
-            ),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text(
+          'Hair Salon',
+          ),
         ),
-        Container(
-          alignment: Alignment.center,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/salonP.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
           padding: EdgeInsets.all(10),
-          child: Text(
-            'Sign In',
-            style: TextStyle(fontSize: 20),
+          child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10), 
+              child: Text(
+                'Hair Salon',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                ),
+                ),
             ),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          child: TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'User Name',
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Sign In',
+                style: TextStyle(fontSize: 20),
+                ),
             ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-          child: TextField(
-            obscureText: true,
-            controller: passwordController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          ),
-        ),
-       TextButton(
-        onPressed: (){}, 
-        child: Text(
-          'Forget Password',
-          ),
-        ),
-        Container(
-          height: 50,
-          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: ElevatedButton(
-            onPressed: (){
-              print(nameController.text);
-              print(passwordController.text);
-               Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-            }, 
-            child: Text('Log In'),),
-        ),
-      Row(
-        children: <Widget>[
-          Text('Does not have a account ?'),
-          TextButton(
-            onPressed: (){
-             Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
-            },
-            child: Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
               ),
             ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),   
-      ],
-    ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+           TextButton(
+            onPressed: (){}, 
+            child: Text(
+              'Forget Password',
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                onPressed: (){
+                  print(nameController.text);
+                  print(passwordController.text);
+                   Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+                }, 
+                child: Text('Log In'),),
+            ),
+          Row(
+            children: <Widget>[
+              Text('Does not have a account ?'),
+              TextButton(
+                onPressed: (){
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 20),
+                  ),
+                ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),   
+          ],
+        ),
+        ),
+      ),
     );
   }
 }
